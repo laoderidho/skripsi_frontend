75dd1bbc589a57c4d61c86e32788f8739875f1d6
laoderidho <2081007@unai.edu>
2023-10-03 11:30:26 +0700

    update add login

---
+++ cusr-unai/src/components/menu/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function login() {
+  return (
+    <div className='blue-login'>
+        <div class="card-login">
+            
+        </div>
+    </div>
+  )
+}
--- cusr-unai/src/routes/Router.jsx
+++ cusr-unai/src/routes/Router.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter , Routes, Route} from 'react-router-dom'
 import Home from '../Pages/Home'
+import Login from '../components/menu/Login'
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/"  element={<Home/>} />
+        <Route path="/login"  element={<Login/>} />
       </Routes>
     </BrowserRouter>
   )
