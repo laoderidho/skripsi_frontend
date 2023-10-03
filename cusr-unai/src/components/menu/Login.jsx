c6681b5d703c569c2367d1a4eb6df33e385a57cf
sharonwav <2082023@unai.edu>
2023-10-03 16:56:16 +0700

    modified form login

--- cusr-unai/src/components/menu/Login.jsx
+++ cusr-unai/src/components/menu/Login.jsx
@@ -6,7 +6,7 @@ export default function login() {
   return (
     <div className="blue-login d-flex justify-content-center align-items-center">
       <div class="card-login bg-white rounded">
-      <div class="image-logo container pt-5 pb-3 d-flex justify-content-between">
+      <div class="image-logo container pt-5 pb-3 d-flex justify-content-evenly">
           <img src="./assets/img/logo.svg" alt="tes" className='w-40'/>
           <strong className='pt-2'>Klinik Universitas Advent Indonesia</strong>
       </div>
@@ -20,7 +20,7 @@ export default function login() {
           <Form.Control type="password" placeholder="Enter username" />
         </Form.Group>
         <Button variant="primary" type="submit" className='w-100 mt-3'>
-          Submit
+          Login
         </Button>
         </Form>
       </div>
