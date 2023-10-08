9a21c30e60a638a7787c4c2255572b8063dd2f06
laoderidho <2081007@unai.edu>
2023-10-08 10:57:13 +0700

    modified sidebar

--- cusr-unai/src/components/menu/Sidebar.jsx
+++ cusr-unai/src/components/menu/Sidebar.jsx
@@ -9,27 +9,65 @@ export default function Sidebar(){
     }
 
     return (
-        <div>
-            <nav class="navbar navbar-expand-lg">
-                <div class="container-fluid shadow-sm">
-                    <button class="btn sidebarbutton" onClick={getClick} type="button" data-toggle="collapse" data-target>
-                        <svg width="25" viewBox="0 0 25 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect y="0.1698" width="25" height="4.13208" rx="2.06604" fill="#1F1E5B"/>
-                            <rect y="8.30188" width="25" height="4.13208" rx="2.06604" fill="#1F1E5B"/>
-                            <rect y="16.434" width="25" height="4.13208" rx="2.06604" fill="#1F1E5B"/>
-                        </svg>
-                    </button>
-                </div>
-            </nav>  
-            {/* conditional sidebar */}
-            {sidebar ? (<div className="sidebar">
+      <div>
+        <nav class="navbar navbar-expand-lg">
+          <div class="container-fluid shadow-sm">
+            <button
+              class="btn sidebarbutton"
+              onClick={getClick}
+              type="button"
+              data-toggle="collapse"
+              data-target
+            >
+              <svg
+                width="25"
+                viewBox="0 0 25 21"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  y="0.1698"
+                  width="25"
+                  height="4.13208"
+                  rx="2.06604"
+                  fill="#1F1E5B"
+                />
+                <rect
+                  y="8.30188"
+                  width="25"
+                  height="4.13208"
+                  rx="2.06604"
+                  fill="#1F1E5B"
+                />
+                <rect
+                  y="16.434"
+                  width="25"
+                  height="4.13208"
+                  rx="2.06604"
+                  fill="#1F1E5B"
+                />
+              </svg>
+            </button>
+          </div>
+        </nav>
+        {/* conditional sidebar */}
+        {/* {sidebar ? (<div className="sidebar">
                 <a href="#">Profile</a>
                 <a href="#">Daftar Pasien</a>
                 <a href="#">Laporan</a>
                 <a href="#">Pemeriksaan Awal</a>
                 <a href="#">Ganti Kata Sandi</a>
                 <a href="#">Logout</a>
-            </div>) : (<div></div>)}
-        </div>  
+            </div>) : (<div></div>)} */}
+
+        <div className={`sidebar ${sidebar ? "" : "sidebar-false"}`}>
+          <a href="#">Profile</a>
+          <a href="#">Daftar Pasien</a>
+          <a href="#">Laporan</a>
+          <a href="#">Pemeriksaan Awal</a>
+          <a href="#">Ganti Kata Sandi</a>
+          <a href="#">Logout</a>
+        </div>
+      </div>
     );
 }
