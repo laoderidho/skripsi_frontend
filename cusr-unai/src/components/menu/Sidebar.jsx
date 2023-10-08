da80e4e8a620b0c1102063f2430874f0eff01a4d
sharonwav <2082023@unai.edu>
2023-10-08 10:59:13 +0700

    sidebar changes

--- cusr-unai/src/components/menu/Sidebar.jsx
+++ cusr-unai/src/components/menu/Sidebar.jsx
@@ -4,7 +4,7 @@ export default function Sidebar(){
 
     const [sidebar, setSidebar] = useState(false);
 
-    const getClick = () => {
+    const toggleSidebar = () => {
         setSidebar(!sidebar);
     }
 
@@ -12,7 +12,7 @@ export default function Sidebar(){
         <div>
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid shadow-sm">
-                    <button class="btn sidebarbutton" onClick={getClick} type="button" data-toggle="collapse" data-target>
+                    <button class="btn sidebarbutton" onClick={toggleSidebar} type="button" data-toggle="collapse" data-target>
                         <svg width="25" viewBox="0 0 25 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect y="0.1698" width="25" height="4.13208" rx="2.06604" fill="#1F1E5B"/>
                             <rect y="8.30188" width="25" height="4.13208" rx="2.06604" fill="#1F1E5B"/>
@@ -22,14 +22,14 @@ export default function Sidebar(){
                 </div>
             </nav>  
             {/* conditional sidebar */}
-            {sidebar ? (<div className="sidebar">
+            <div className={`sidebar ${sidebar ? 'open' : ''}}`}>
                 <a href="#">Profile</a>
                 <a href="#">Daftar Pasien</a>
                 <a href="#">Laporan</a>
                 <a href="#">Pemeriksaan Awal</a>
                 <a href="#">Ganti Kata Sandi</a>
                 <a href="#">Logout</a>
-            </div>) : (<div></div>)}
+            </div>
         </div>  
     );
 }
