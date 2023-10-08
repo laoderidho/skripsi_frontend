44703fc19bbab7f20966fef6e20064c5ac6df987
sharonwav <2082023@unai.edu>
2023-10-08 20:28:11 +0700

    daftar pasien border

--- cusr-unai/src/routes/Router.jsx
+++ cusr-unai/src/routes/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter , Routes, Route} from 'react-router-dom'
 import Home from '../Pages/Home'
 import Login from '../components/menu/Login'
 import Sidebar from '../components/menu/Sidebar'
+import DaftarPasien from '../components/menu/DaftarPasien'
 
 export default function Router() {
   return (
@@ -11,6 +12,7 @@ export default function Router() {
         <Route path="/"  element={<Home/>} />
         <Route path="/login"  element={<Login/>} />
         <Route path="/sidebar" element={<Sidebar/>}/>
+        <Route path="/daftarpasien" element={<DaftarPasien/>}/>
       </Routes>
     </BrowserRouter>
   )
