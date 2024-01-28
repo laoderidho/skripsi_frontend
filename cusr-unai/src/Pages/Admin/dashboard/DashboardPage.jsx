d7aa32562a0bed6c23a3bfa20fda93438795102b
elizabethgeraldine <94831720+laoderidho@users.noreply.github.com>
2024-01-28 20:44:57 +0700

    modified dashboard page

--- cusr-unai/src/Pages/Admin/dashboard/DashboardPage.jsx
+++ cusr-unai/src/Pages/Admin/dashboard/DashboardPage.jsx
@@ -1,42 +1,13 @@
-import React,{useState, useEffect} from 'react'
-import axios from '../../../axios'
-import AuthorizationRoute from '../../../AuthorizationRoute'
+import React,{} from 'react'
+
 import Sidebar from '../../../components/menu/Sidebar'
 
 export default function DashboardPage() {
 
-    const [users, setUsers] = useState([])
-
-    const getDataUser = async (token) => {
-        try {
-        await axios
-            .post("/admin", {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-            })
-            .then((res) => {
-            console.log(res)
-            setUsers(res?.data?.data);
-            });
-        } catch (error) {
-            AuthorizationRoute(error.response.status);
-        }
-  };
-
-  useEffect(()=>{
-    getDataUser(localStorage.getItem('token'))
-  }, [])
-
-  console.log(users)
 
   return (
       <Sidebar>
-        {users.map((item, index) => (
-          <div key={index}>
-            <h1>{item.nama}</h1>
-          </div>
-        ))}
+       
       </Sidebar>
   
   );
