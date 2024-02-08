bb7960cc9a9ae92605711c2b0726dd6c3640893c
sharonwav <2082023@unai.edu>
2024-02-08 14:27:26 +0700

    Merge branch 'sandbox/ridho' of https://github.com/laoderidho/skripsi_frontend into sandbox/sharon

--- cusr-unai/src/Pages/Perawat/form-ski/FormEvaluasi.jsx
+++ cusr-unai/src/Pages/Perawat/form-ski/FormEvaluasi.jsx
@@ -9,12 +9,104 @@ import ConfirmModal from '../../../components/menu/ConfirmModal';
 
 export default function FormEvaluasi() {
 
-    const [evaluasi, setEvaluasi] = useState([]);
+    const [luaran, setLuaran] = useState([]);
+    const [catatan, setCatatan] = useState('');
+
+    const [selectedLuaran, setSelectedLuaran] = useState('');
+    const [selectedKriteriaLuaran, setSelectedKriteriaLuaran] = useState([]);
+
+    // VALUE
+
+    const [nama_kriteria_luaran, setNamaKriteriaLuaran] = useState(null);
+
+    const createLuaranOptions = () => {
+        if (!luaran || luaran.length === 0) {
+            return [
+                { value: '', label: 'Pilih Luaran'}
+            ];
+        } else {
+            const options = [
+                { value: '', label: '-'}
+            ];
+
+            luaran.forEach((item, index) => {
+                options.push({
+                    value: item.id,
+                    label: `${item.kode_luaran} - ${item.nama_luaran}`
+                });
+            });
+            return options;
+        }
+    };
+
+    const token = localStorage.getItem('token');
+
+    const handleLuaranChange = async () => {
+        try {
+            const res = await axios.post(
+                `perawat/luaran/detail/${selectedLuaran}`,
+                {
+                    headers: { Authorization: `Bearer ${localStorage.getItem(token)}`,
+                },
+                }
+            );
+
+            const selectedLuaranData = res.data;
+
+            setSelectedKriteriaLuaran(selectedLuaranData.nama_kriteria_luaran);
+        } catch (error) {
+
+        }
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await handleLuaranChange(selectedLuaran);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    },[selectedLuaran])
 
 
     return (
         <Sidebar>
+            <div className='container'>
+                <h2>Form Luaran</h2>
+            </div>
 
+            <div className='container'>
+                <Form className='container'>
+                    <Form.Group className='mt-4'>
+                        <Form.Label>Luaran</Form.Label>
+                        <Dropdown
+                            value={selectedLuaran}
+                            onChange={(e) => setSelectedLuaran(e.target.value)}
+                            options={createLuaranOptions()}
+                            placeholder='Pilih Luaran'
+                            filter
+                            required
+                            className='pt-1'>
+                        </Dropdown>
+
+                        <Form.Group className='mt-5'>
+                            <Form.Label>Kriteria</Form.Label>
+                            <MultiSelect
+                                value={nama_kriteria_luaran}
+                                onChange={(e) => setNamaKriteriaLuaran(e.target)}
+                                options={selectedKriteriaLuaran}
+                                disabled={!selectedLuaran}
+                                placeholder='Pilih Kriteria'
+                                optionlLabel='nama'>
+
+                            </MultiSelect>
+                        </Form.Group>
+                    </Form.Group>
+                </Form>
+            </div>
         </Sidebar>
     )
 }
