398c16969bcb1495ee8d295c279ce46780ffcd3f
sharonwav <2082023@unai.edu>
2024-05-02 20:59:41 +0700

    daftar pasien

--- cusr-unai/src/components/menu/LoadingSKI.jsx
+++ cusr-unai/src/components/menu/LoadingSKI.jsx
@@ -3,15 +3,7 @@ import Sidebar from "../../../../components/menu/SidebarAdmin";
 
 export default function LoadingSKI() {
 
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 1500);
-
-        return () => clearTimeout(timer);
-    }, [])
+    
     return (
         <Sidebar>
             
